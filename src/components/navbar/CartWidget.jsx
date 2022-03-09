06d6425b4e5c06a8354162e38a8b176cb7cbbd1d
Pablo Rapalin <pablo.rapalin@gmail.com>
2022-03-09 01:33:25 -0300

    fix styles for navbar

--- src/components/navbar/CartWidget.jsx
+++ src/components/navbar/CartWidget.jsx
@@ -1,13 +1,15 @@
 import React from 'react'
-import 'bootstrap/dist/css/bootstrap.min.css';
+import { ShoppingCartIcon } from "@heroicons/react/solid";
 
 const CartWidget = () => {
   return (
-    <div className='shoping-cart'>
-      <svg xmlns="http://www.w3.org/2000/svg" class="h-6 w-6" fill="none" viewBox="0 0 24 24" stroke="currentColor">
-        <path stroke-linecap="round" stroke-linejoin="round" stroke-width="2" d="M3 3h2l.4 2M7 13h10l4-8H5.4M7 13L5.4 5M7 13l-2.293 2.293c-.63.63-.184 1.707.707 1.707H17m0 0a2 2 0 100 4 2 2 0 000-4zm-8 2a2 2 0 11-4 0 2 2 0 014 0z" />
-      </svg>
+    <div className='relative shoping-cart h-6 w-6'>
+      <ShoppingCartIcon  />
+      <div className='absolute inset-5 bg-red-500 text-gray-100 rounded-full text-size-1 w-4 h-4 text-center align-middle flex justify-center items-center'>{0}</div>
     </div>
+
+
+
   )
 }
 
